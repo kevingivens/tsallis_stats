ffdc6153d8496ed7ce1f0272222a6cbaf7649d09
Kevin Givens <givenskevinm@gmail.com>
2021-11-25 11:56:10 -0500

    add test

---
+++ tests/test_tsallis.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pytest
+from scipy import stats
+from scipy.stats.tests.test_continuous_basic import (
+    check_sample_meanvar_, 
+    cases_test_cont_basic
+)
+
+@pytest.mark.parametrize('distname,arg', cases_test_cont_basic())
+@pytest.mark.parametrize('sn, n_fit_samples', [(500, 200)])
+def test_cont_basic(distname, arg, sn, n_fit_samples):
+    # this test skips slow distributions
+
+    try:
+        distfn = getattr(stats, distname)
+    except TypeError:
+        distfn = distname
+        distname = 'rv_histogram_instance'
+
+    rng = np.random.RandomState(765456)
+    rvs = distfn.rvs(size=sn, *arg, random_state=rng)
+    sm = rvs.mean()
+    sv = rvs.var()
+    m, v = distfn.stats(*arg)
+
+    check_sample_meanvar_(distfn, arg, m, v, sm, sv, sn, distname + 'sample mean test')
