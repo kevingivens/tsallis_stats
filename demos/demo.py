a51de0c779817f0a9c210cb7ec02b1eb853f27d5
Kevin Givens <givenskevinm@gmail.com>
2024-01-06 18:38:15 -0500

    update demos

--- demos/demo.py
+++ demos/demo.py
@@ -1,19 +1,11 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
 from tsallis_stats.tsallis import q_gaussian
 
-if __name__ == "__main__":
-    q, beta = 1.2, 3
-    mean, var, skew, kurt = q_gaussian.stats(q, beta, moments='mvsk')
-    
-    print(mean, var)
-    # print(q_gaussian.stats(q, beta, moments='m'))
-    #fig, ax = plt.subplots(1, 1)
-    #x = np.linspace(q_gaussian.ppf(0.01, q, beta), q_gaussian.ppf(0.99, q, beta), 100)
-    #ax.plot(x, q_gaussian.pdf(x, q, beta, 'r-', lw=5, alpha=0.6, label='q_gaussian pdf'))
-    rvs = q_gaussian.rvs(q, beta, size=10000)
-    print(rvs.mean(), rvs.var())
-    #ax.hist(r, density=True, histtype='stepfilled', alpha=0.2)
-    #ax.legend(loc='best', frameon=False)
-    #plt.show()
+q, beta = 1.2, 3
+mean, var, skew, kurt = q_gaussian.stats(q, beta, moments='mvsk')
+print(mean, var)
+print(q_gaussian.stats(q, beta, moments='m'))
+
+rvs = q_gaussian.rvs(q, beta, size=10000)
+print(rvs.mean(), rvs.var())
